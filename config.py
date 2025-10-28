7ccb16f3822dd453da508adcb972d742d98ee412
Harsha Vasinenku <harsha.vasinenku@gmail.com>
2025-10-28 15:55:31 -0400

    config.py

--- config.py
+++ config.py
@@ -1,23 +1,16 @@
 import os
 
+# --- Database Configuration for psycopg2 ---
+DB_HOST = os.environ.get('DB_HOST', 'TBD')
+DB_PORT = os.environ.get('DB_PORT', 5432)
+DB_NAME = os.environ.get('DB_NAME', 'TBD')
+DB_USER = os.environ.get('DB_USER', 'TBD')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', 'TBD')
+SCHEMA = "TBD"
 
-SQLALCHEMY_DATABASE_URI = os.environ.get(
-    "DATABASE_URI",
-    "postgresql://user:password@localhost:5432/saayam_local_db"
-)
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+# --- Geolocation Service Configuration ---
+NOMINATIM_USER_AGENT = 'SaayamForAll/1.0 (info@saayam.com)'
 
 # --- Volunteer Search Configuration ---
-DEFAULT_SEARCH_LIMIT = 5
-MIN_SEARCH_RADIUS_KM = 0
-MAX_SEARCH_RADIUS_KM = 1000
-
-
-# --- Geolocation Service and Tile System Configuration ---
-NOMINATIM_USER_AGENT = 'SaayamForAll/1.0 (info@saayam.com)'
-EARTH_RADIUS_KM = 6371  # Earth's radius in kilometers
-S2_LEVEL = 5  # Size of each tile in s2 (6k cells for the whole world)
-
-
+DEFAULT_RADIUS_KM = 25
+DEFAULT_LIMIT = 10
